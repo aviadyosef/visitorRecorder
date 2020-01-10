dfe1f905338ac336979a70688ff5d529f341f09b
Aviad Yosef <aviadyos@gmail.com>
2020-01-11 01:20:34 +0200

    first sprint boot outOfTheBox commit.

---
+++ src/main/java/com/asyh/visitorRecorder/VisitorRecorderApplication.java
@@ -0,0 +1,13 @@
+package com.asyh.visitorRecorder;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VisitorRecorderApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VisitorRecorderApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/asyh/visitorRecorder/VisitorRecorderApplicationTests.java
@@ -0,0 +1,13 @@
+package com.asyh.visitorRecorder;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VisitorRecorderApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
